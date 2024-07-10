32355151958d327df3f56894255520289e1dc147
Benjamin Owen <benjamin@projecttiy.com>
2024-07-10 09:54:51 -0400

    added in a class to handle argument parsing

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
+import sys
+
+from src.classes.parseargs import ParseArgs
+
 
 def main():
-    pass
+    args = sys.argv
+    myparser = ParseArgs(args)
 
 
 if __name__ == '__main__':
