ac33483690f8814243825831effb40099d9dd6ff
Benjamin Owen <benjamin@projecttiy.com>
2024-07-10 15:04:08 -0400

    added jellyfin class and unit tests for jellyfin and filechecker classes

---
+++ src/classes/jellyfin.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+import requests
+
+from src.classes.file_checker import FileChecker
+from src.constants import constants
+
+
+class Jellyfin:
+    SCHEME = constants.JELLYFIN_API_SCHEME
+
+    def __init__(self, credentials_file: str) -> None:
+        self.headers = {
+            'Accept': 'application/json'
+        }
+        self.credentials_file = credentials_file
+
+    @property
+    def credentials_file(self) -> str:
+        return self._credentials_file
+
+    @credentials_file.setter
+    def credentials_file(self, filepath: str) -> None:
+        self._credentials_file = ''
+        fc = FileChecker(filepath)
+        if not fc.is_file():
+            raise ValueError('Not a file')
+
+        if not fc.is_readable():
+            raise ValueError('Not readable')
+
+        data = fc.is_yaml()
+        if not data:
+            raise ValueError('Not YAML')
+
+        try:
+            credentials = data['credentials']  # type: ignore
+            self._credentials_file = fc.file
+            self.host = credentials['host']
+            self.apikey = credentials['apikey']
+
+            self.headers['Host'] = self.host
+            self.headers['X-Emby-Token'] = self.apikey
+        except KeyError:
+            raise ValueError('Invalid credentials file')
+
+    def test(self) -> bool:
+        endpoint = '/System/Info'
+        url = self.SCHEME + self.host + endpoint
+        r = requests.get(url=url, headers=self.headers)
+        if r.status_code == 401:
+            print('401 Unauthorized')
+            return False
+        if r.status_code != 200:
+            data = r.json()
+            data['status_code'] = r.status_code
+            print(', '.join('{}: {}'.format(key, value)
+                  for key, value in data.items()))
+            return False
+        return True
---
+++ tests/test_jellyfin.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python3
+import os
+from re import template
+
+import pytest
+
+from src.classes.jellyfin import Jellyfin
+from src.constants import constants
+
+
+class TestJellyfin:
+    def setUp(self):
+        self.credentials_file = 'tests/data/credentials.yaml'
+        self.jf = Jellyfin(self.credentials_file)
+
+    def tearDown(self):
+        del self.jf
+        del self.credentials_file
+
+    def test_missing_credentials_file(self):
+        file = '/some/fake/file'
+        with pytest.raises(ValueError):
+            Jellyfin(file)
+
+    def test_invalid_credentials_file_not_a_file(self):
+        file = '~/Documents/'
+        with pytest.raises(ValueError):
+            Jellyfin(file)
+
+    def test_invalid_credentials_file_not_readable(self, monkeypatch):
+        def mock_return(path, mode):
+            return False
+        monkeypatch.setattr(os, 'access', mock_return)
+        file = 'tests/data/credentials.yaml'
+        with pytest.raises(ValueError):
+            Jellyfin(file)
+
+    def test_invalid_credentials_file_not_yaml(self):
+        file = 'tests/data/not_yaml.yaml'
+        with pytest.raises(ValueError):
+            Jellyfin(file)
+
+    def test_invalid_credentials_file_invalid_file(self):
+        file = 'tests/data/invalid_credentials.yaml'
+        with pytest.raises(ValueError):
+            Jellyfin(file)
+
+    def test_invalid_credentials_file_missing_key(self):
+        file = 'tests/data/missing_credentials.yaml'
+        with pytest.raises(ValueError):
+            Jellyfin(file)
+
+    def test_invalid_credentials_file_additional_data(self):
+        file = 'tests/data/additional_credentials.yaml'
+        with pytest.raises(ValueError):
+            Jellyfin(file)
+
+    def test_credentials_file(self):
+        self.setUp()
+        assert self.jf.credentials_file == os.path.realpath(
+            self.credentials_file)
+        assert self.jf.host == 'subdomain.domain.tld'
+        assert self.jf.apikey == '0f219ab50824a2c5de8360c2473eafe4'
+        self.tearDown()
+
+    def test_headers(self):
+        self.setUp()
+        assert len(self.jf.headers) == 3
+        assert 'Accept' in self.jf.headers.keys()
+        assert 'Host' in self.jf.headers.keys()
+        assert 'X-Emby-Token' in self.jf.headers.keys()
+        assert self.jf.headers['Accept'] == 'application/json'
+        assert self.jf.headers['Host'] == 'subdomain.domain.tld'
+        assert self.jf.headers['X-Emby-Token'] == '0f219ab50824a2c5de8360c2473eafe4'
+        self.tearDown()
+
+    def test_test_failed_unauthenticated(self, requests_mock):
+        self.setUp()
+        endpoint = '/System/Info'
+        url = constants.JELLYFIN_API_SCHEME + self.jf.host + endpoint
+        status_code = 401
+        requests_mock.register_uri(
+            'GET', url, text='', status_code=status_code)
+        result = self.jf.test()
+        assert result is False
+        self.tearDown()
+
+    def test_test_failed_forbidden(self, requests_mock):
+        self.setUp()
+        endpoint = '/System/Info'
+        url = constants.JELLYFIN_API_SCHEME + self.jf.host + endpoint
+        status_code = 403
+        with open('tests/data/403_response.json', 'r') as file:
+            data = file.read()
+        requests_mock.register_uri(
+            'GET', url, text=data, status_code=status_code)
+        result = self.jf.test()
+        assert result is False
+        self.tearDown()
+
+    def test_test(self, requests_mock):
+        self.setUp()
+        endpoint = '/System/Info'
+        url = constants.JELLYFIN_API_SCHEME + self.jf.host + endpoint
+        status_code = 200
+        with open('tests/data/info_response.json', 'r') as file:
+            data = file.read()
+        requests_mock.register_uri(
+            'GET', url, text=data, status_code=status_code)
+        result = self.jf.test()
+        assert result is True
+        self.tearDown()
